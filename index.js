18d1bca1777f14f6c68b53a67aafcedd66d66345
Shermin Sultana <rima64549@gmail.com>
2023-09-05 02:09:34 +0600

    add

--- index.js
+++ index.js
@@ -19,3 +19,22 @@ const promptBtn =()=>{
     const result = prompt("check your phone");
     console.log(result);
 }
+
+
+/*function errors(a){
+    if(a>5){
+        return true
+    }else{
+        return false
+    }
+console.log(errors(s)).....syntex error*/
+
+/*function text(){
+    var str = "Hello world";
+    return str;
+}
+console.log(str);......refference error */
+
+
+/*var foo;
+console.log(foo.bar);*/
