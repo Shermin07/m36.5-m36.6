e64c964402d405781bcf8b866a442a86dec0684e
Shermin Sultana <rima64549@gmail.com>
2023-09-04 06:04:51 +0600

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function alertBtn(){
+    alert("hello world")
+}
+
+const moreAlert= ()=>{
+    alert("be attentive your class")
+}
+
+const confirmBtn =() =>{
+    //confirm("are you ready?");
+    const result = confirm("are you ready?");
+    console.log(result);
+
+
+}
+
+const promptBtn =()=>{
+    const result = prompt("check your phone");
+    console.log(result);
+}
